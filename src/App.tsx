3bf1cef291bfb60e0c1021f3e30e91ce57760af0
Michael <michael.m.nguyen@live.com>
2024-11-10 00:28:05 -0600

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,123 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import {MantineProvider, Button, MultiSelect} from '@mantine/core';
+import '@mantine/core/styles.css';
+
 import './App.css';
 
-function App() {
+import vietVoc from './cards.json'
+
+interface Card {
+  Aside: string;
+  Bside: string;
+  categories: string[];
+}
+
+interface FlashCardsProps {
+  cards: Card[];
+}
+
+interface CategoriesDropdown {
+  cards: Card[];
+  handleSelectedCategories: (categories: string[]) => void;
+}
+
+const getRandomCard = (cards: Card[]):Card => {
+  let randIndex = Math.floor(Math.random() * cards.length);
+  let randCard = cards[randIndex];
+
+  return randCard;
+}
+
+const CategoriesDropdown = (props: CategoriesDropdown) => {
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([])
+
+  const categories = Array.from(new Set(props.cards.flatMap(card => card.categories)));
+
+  const handleCategoriesSelected = (categories: string[]) => {
+    setSelectedCategories(categories);
+    props.handleSelectedCategories(categories);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <MultiSelect
+        label="Categories"
+        placeholder="Filter Cards By a Category"
+        data={categories}
+        value={selectedCategories}
+        onChange={handleCategoriesSelected}
+      />
+    </>
+  )
+}
+
+const FlashCards = (props: FlashCardsProps) => {
+  const [currentCard, setCurrentCard] = useState<Card>(getRandomCard(props.cards))
+  const [ShowFront, setShowFront] = useState<boolean>(true);
+  const [FrontIsA, setFrontIsA] = useState<boolean>(true);
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+
+  const handleNextCard = () => {
+    setShowFront(true);
+    console.log(props.cards.length);
+    const filteredCards = selectedCategories.length === 0 
+        ? props.cards 
+        : props.cards.filter(card => 
+            card.categories.some(category => selectedCategories.includes(category))
+        );
+
+    console.log(filteredCards.length);
+
+    setCurrentCard(getRandomCard(filteredCards));
+  }
+
+  const handleFlipDeck = () => {
+    setFrontIsA(!FrontIsA);
+  }
+
+  const handleFlipCard = () => {
+    setShowFront(!ShowFront);
+  }
+
+  const handleSelectCategories = (categories: string[]) => {
+    setSelectedCategories(categories);
+  }
+
+  return (
+      <>
+      <div className="cards-wrapper">
+        <CategoriesDropdown 
+          cards={props.cards}
+          handleSelectedCategories={handleSelectCategories}
+        />
+        <div className="card-container"
+          onClick={handleFlipCard}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {((FrontIsA && ShowFront) || (!FrontIsA && !ShowFront)) &&(
+            currentCard.Aside
+          )}
+          {((FrontIsA && !ShowFront) || (!FrontIsA && ShowFront)) &&(
+            currentCard.Bside
+          )}
+        </div>
+        <Button onClick={handleNextCard}>Next Card</Button>
+        <br/>
+        <Button onClick={handleFlipDeck}>Flip Deck</Button>
+      </div>
+      </>
+  )
+}
+
+function App() {
+
+
+  return (
+    <>
+    <MantineProvider>
+      <FlashCards cards={vietVoc} />
+    </MantineProvider>
+    </>
   );
 }
 
---
+++ src/Flashcards.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import vietVoc from './cards.json'
+
+interface Word {
+    Aside: string;
+    Bside: string;
+}
+
+
+interface FlashCardsProps {
+    words: Word[];
+}
+
+const FlashCards = () => {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default FlashCards
