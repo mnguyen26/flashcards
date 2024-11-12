20869e0f344538845f901ffe415853bfb6e24331
Michael <michael.m.nguyen@live.com>
2024-11-11 22:36:56 -0600

    matching logic

--- src/Matching.tsx
+++ src/Matching.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import { Card } from './App';
 import './App.css';
@@ -32,30 +32,82 @@ const getCards = (deck: Card[], numCards: number):Card[] => {
   return cards;
 }
 
-const MatchGame = (props: MatchGameProps) => {
-  const [currentCards, setCurrentCards] = useState<Card[]>(getCards(props.cards, 5));
+// MMN re-write this how you would
+const getRandIndexes = ():number[] => {
+  const indexes: number[] = [0, 1, 2, 3, 4];
+  
+  for (let i=4 ; i>0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [indexes[i], indexes[j]] = [indexes[j], indexes[i]];
+  }
+  
+  return indexes;
+}
 
-  const getRandIndexes = ():number[] => {
-    const indexes: number[] = [1, 2, 3, 4, 5];
-    
-    for (let i=4 ; i>0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [indexes[i], indexes[j]] = [indexes[j], indexes[i]];
+const MatchGame = (props: MatchGameProps) => {
+  const [cardsLeft, setCardsleft] = useState<Card[]>(getCards(props.cards, 5));
+  const [cardsRight, setCardsRight] = useState<Card[]>([]);
+  const [selectedCardLeft, setSelectedCardLeft] = useState<Card|null>(null);
+  const [selectedCardRight, setSelectedCardRight] = useState<Card|null>(null);
+
+  useEffect(() => {
+    const cards: Card[] = [];
+    const indexes = getRandIndexes();
+
+    for (let i=0; i<cardsLeft.length; i++) {
+      cards.push(cardsLeft[indexes[i]]);
     }
 
-    return indexes;
+    setCardsRight(cards);
+  },[cardsLeft])
+
+  useEffect(() => {
+    if (selectedCardLeft !== null && selectedCardRight !== null) {
+      if (selectedCardLeft.Aside === selectedCardRight.Aside) {
+        console.log("MATCH!");
+      } else {
+        console.log("no");
+      }
+      setSelectedCardLeft(null);
+      setSelectedCardRight(null);
+    }
+  }, [selectedCardLeft, selectedCardRight]);
+
+  const handleSetCard = (card: Card, leftCard: boolean) => {
+    if (leftCard) {
+      setSelectedCardLeft(card);
+    } else {
+      setSelectedCardRight(card);
+    }
   }
 
   return (
     <>
-    <div> 
-      {currentCards.map((card: Card) => {
-        return (
-          <div className="match-card">
-            {card.Aside}
-          </div>
-        )
-      })}
+    <div className="matching-wrapper">
+      <div className="matching-container">
+        <div className="matching-col">
+          {cardsLeft.map((card: Card) => {
+            return (
+              <div 
+              className="match-card"
+              onClick={() => handleSetCard(card, true)}>
+                {card.Aside}
+              </div>
+            )
+          })}
+        </div>
+        <div className="matching-col">
+        {cardsRight.map((card: Card) => {
+            return (
+              <div 
+              className="match-card"
+              onClick={() => handleSetCard(card, false)}>
+                {card.Bside}
+              </div>
+            )
+          })}
+        </div>
+      </div> 
     </div>
     </>
   )
